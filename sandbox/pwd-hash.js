4e465db775707e807dbbb413774bb67eac46eadf
Michaël <afmichael73@gmail.com>
2022-06-25 14:49:51 +0300

    -- typos + pwd hash genrator

---
+++ sandbox/pwd-hash.js
@@ -0,0 +1,16 @@
+const crypto = require ('crypto');
+
+/**
+ * Generate a password hash value for configuring the backoffice
+ * @usage
+ * node path/pwd-hash your_password 
+ */
+const [ , , password] = process.argv;
+
+const hash = crypto
+    .createHash('sha256')
+    .update(password)
+    .digest('hex');
+
+console.log ("Generated hash value :");
+console.log (hash);
